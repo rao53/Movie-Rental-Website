e0c0750c0377c9937cea68dc6ee86f97208cc792
Ridwan <rao53@drexel.edu>
2019-04-03 13:15:36 -0400

    Add a new column for the customer entity and updated it with some data.

--- iPipeMR/Controllers/CustomersController.cs
+++ iPipeMR/Controllers/CustomersController.cs
@@ -5,39 +5,46 @@ using System.Web;
 using System.Web.Mvc;
 using iPipeMR.Models;
 using iPipeMR.ViewModel;
+using System.Data.Entity;
 
 namespace iPipeMR.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
-/*
-        [Route("Customers/")] 
-*/
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+/*Note that .Include is used here to ensure that all related objects with customer are queried when this object is to be displayed in the view*/
+            var customers = _context.Customers.Include(c => c.MembershipType);
+/*
+ It should be noted that we can immediately execute a query on this method by doing the below
+            var customers = _context.Customers.toList();
+*/
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+/*Note that our query is immediately executed here due to the single or default method attached to it*/
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
         }
     }
 }
--- iPipeMR/Models/Customer.cs
+++ iPipeMR/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,9 @@ namespace iPipeMR.Models
 {
     public class Customer
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
