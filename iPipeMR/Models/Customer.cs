c62b519ede0b7a181bb86f140f7045dd20b07af9
Ridwan <rao53@drexel.edu>
2019-04-03 11:32:26 -0400

    AddMembershipType

--- iPipeMR/Models/Customer.cs
+++ iPipeMR/Models/Customer.cs
@@ -7,7 +7,10 @@ namespace iPipeMR.Models
 {
     public class Customer
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
